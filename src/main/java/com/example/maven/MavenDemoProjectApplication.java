b4db03a53e53c452ebe657a697b1bc110dcf1146
Sanj1yKum1rSin1h <Sanjay.kumar.bng03@gmail.com>
2026-02-20 14:48:38 +0530

    test 4

--- src/main/java/com/example/maven/MavenDemoProjectApplication.java
+++ src/main/java/com/example/maven/MavenDemoProjectApplication.java
@@ -1,16 +1,16 @@
 package com.example.maven;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MavenDemoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenDemoProjectApplication.class, args);
-		System.out.println("Jenkins integration...");
-		System.out.println("Test 1");
-		System.out.println("test 2 running");
+		log.info("Test 1 github repo integrated with jenkins");
 	}
 
 }
